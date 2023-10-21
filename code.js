309d726d8bfc9d5963b01ee7e4e04498a2f7f78a
Torazo5 <torazo.tokuda@gmail.com>
2023-10-21 16:22:09 +0800

    shits tough

--- code.js
+++ code.js
@@ -1,17 +1,23 @@
-let dimension = 16
-const container = document.getElementById('gridContainer');
-  // Create 256 grid squares (16x16)
-  for (let i = 0; i < (dimension*dimension); i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    gridSquare.addEventListener('mouseover', () => {
-        gridSquare.style.backgroundColor = 'black';
-    });
-    container.appendChild(gridSquare);
-  }
-
-const change_dimensions =document.getElementById('change')
-change_dimensions.addEventListener('click', () =>{
-  let dimension = prompt('what dimensions do you want?')
+const changeDimension = document.getElementById('change')
+function createGrid(dimension){                 
+    const flexc = document.getElementById('flex-container'); 
+    flexc.innerHTML = '';  
+    for (let e =0; e<dimension;e++){
+    const fcontainer = document.createElement('div'); 
+    fcontainer.classList.add('flex-container'); 
+    for (let i = 0; i < dimension; i++) {
+        const new_div = document.createElement('div');
+        new_div.addEventListener('mouseover', () =>{
+            console.log('HOVERED')
+            new_div.style.backgroundColor = 'black'
+        })
+        fcontainer.appendChild(new_div);
+    }
+    flexc.appendChild(fcontainer);
+    }
+}
+createGrid(16)
+changeDimension.addEventListener('click', () => {
+    let dimension = prompt('New dimensions:')
+    createGrid(dimension)
 })
-
